ec3da95942198bbe1ec858ddbb2e2b19160999c4
Brad Lucas <brad@beaconhill.com>
2017-05-13 10:09:14 -0400

    initial round of tests

--- test/spammer/core_test.clj
+++ test/spammer/core_test.clj
@@ -1,7 +1,62 @@
 (ns spammer.core-test
   (:require [clojure.test :refer :all]
-            [spammer.core :refer :all]))
+            [spammer.process :as process]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;; (deftest a-test
+;;   (testing "FIXME, I fail."
+;;     (is (= 0 1))))
+
+;; (deftest b-test
+;;   (testing "I succeed"
+;;     (is true)))
+
+(deftest valid-spam-score
+  (testing "valid-spam-core"
+    (is (process/valid-spam-score {:email-address "john123@dired.com" :spam-score process/max-spam-score}))))
+
+
+(deftest valid-running-mean
+  (testing "valid-running-mean"
+    (is (process/valid-running-mean process/max-running-mean))))
+
+(deftest valid-recent-mean
+  (testing "valid-recent-mean"
+    (is (process/valid-recent-mean process/max-mean-recent-100))))
+
+(deftest mean 
+  (testing "mean"
+    (is (= 101/2 (/ (apply + (range 1 101)) 100)))))
+
+(deftest calc-spam-score-mean
+  (testing "calc-spam-score-mean"
+    (is (= 0.5 (process/calc-spam-score-mean [{:spam-score 0.4} {:spam-score 0.6}])))))
+
+(deftest running-mean 
+  (testing "running-mean"
+    (is (= 0.5 (process/running-mean [{:spam-score 0.4} {:spam-score 0.6}])))))
+
+(deftest recent-mean 
+  (testing "TODO - build list of 100+ known records and then test validating the first 100's mean"
+    (is (= 0.5 (process/running-mean [{:spam-score 0.4} {:spam-score 0.6}])))))
+
+(deftest new-email-true
+  "New mail is one that is not in the set of sent-emils which is a list of email-records"
+  (testing "new-mail"
+    (is (process/new-email '({:email-address "test@test.com" :spam-score 0.1}) "foo@foo.com"))))
+
+
+(deftest new-email-false
+  "New mail is one that is not in the set of sent-emils which is a list of email-records"
+  (testing "new-mail"
+    (is (not (process/new-email '({:email-address "foo@foo.com" :spam-score 0.1}) "foo@foo.com")))))
+
+
+;; TODO these need more work...
+(deftest ok-to-send 
+  (testing ""
+    (is true)))
+
+(deftest process-input 
+  (testing ""
+    (is true)))
+
