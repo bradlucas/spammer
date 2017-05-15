4aaad15af389797a8a526e1d8a6c38fd36d408e3
Brad Lucas <brad@beaconhill.com>
2017-05-15 12:08:49 -0400

    added [org.flatland/ordered "1.5.4"]

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojure "1.9.0-alpha16"]]
+                 [org.clojure/clojure "1.9.0-alpha16"]
+                 [org.flatland/ordered "1.5.4"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
   :main spammer.core
 )
