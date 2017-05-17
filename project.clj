0597aa942c3b9350c85bef1e3620a8a1f2f95ccb
Brad Lucas <brad@beaconhill.com>
2017-05-17 07:57:33 -0400

    version 1.0.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject spammer "1.0.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject spammer "1.0.1"
+  :description "Programming assignment to filter emails by spam-score following a set of assigned rules"
+  :url "https://github.com/bradlucas/spammer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
