5b1f1d0139b4831a0d76603ccb6b9beef7c406a4
Brad Lucas <brad@beaconhill.com>
2017-05-17 09:27:59 -0400

    adding timing script

---
+++ timings.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VERSION=1.0.0
+
+time java -jar target/spammer-$VERSION-standalone.jar 10;
+time java -jar target/spammer-$VERSION-standalone.jar 100;
+time java -jar target/spammer-$VERSION-standalone.jar 1000;
+time java -jar target/spammer-$VERSION-standalone.jar 10000;
+time java -jar target/spammer-$VERSION-standalone.jar 100000;
+time java -jar target/spammer-$VERSION-standalone.jar 1000000;
+
+
