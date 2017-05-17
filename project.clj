085cf79e2399a719793cac6b625e6702dff0b4be
Brad Lucas <brad@beaconhill.com>
2017-05-17 08:48:27 -0400

    include test.check so useable from uberjar

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojure "1.9.0-alpha16"]
-                 [org.flatland/ordered "1.5.4"]]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
+                 [org.flatland/ordered "1.5.4"]
+                 [org.clojure/test.check "0.9.0"]]
+  ;;:profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
   :main spammer.core
 )
